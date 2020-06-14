65143b6239cb621e94b272c49ca8755875b0b6fa
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2020-06-14 18:26:27 -0300

    Criando header e buscando produtos da api

---
+++ src/pages/main/index.js
@@ -0,0 +1,23 @@
+import React, {Component } from 'react';
+import api from '../../services/api';
+
+
+export default class Main extends Component{
+componentDidMount(){
+    this.loadProducts();
+}
+
+loadProducts = async () => {
+    const response = await api.get("/products");
+
+    console.log(response);
+};
+
+render(){
+
+    return <h1>Hello Ciro</h1>;
+}
+
+
+
+}
